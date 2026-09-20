9cb91d7a907760f86e6e1d7215d8a7d4b0d1e51b
yurisilva7117-art <yurisilva7117@gmail.com>
2026-09-20 12:40:35 -0300

    Create StoneBreakSpeed.java

---
+++ src/main/java/yuri/stonebreakspeed/StoneBreakSpeed.java
@@ -0,0 +1,9 @@
+package yuri.stonebreakspeed;
+
+import net.fabricmc.api.ModInitializer;
+
+public class StoneBreakSpeed implements ModInitializer {
+    @Override
+    public void onInitialize() {
+    }
+}
